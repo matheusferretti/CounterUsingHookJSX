ddb2e2c69f327b18d9f52c25f161b3d2318e170c
Matheus Monteiro <matheusferretti96@gmail.com>
2020-09-09 23:08:28 +0000

    update1

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 function App() {
-	//returns a pair,
-	//the current state value, and a function that lets update our state
-	const [count, setCount] = useState(() => {
+	function countInitial() {
 		console.log("run function");
 		return 4;
-	});
+	}
+
+	//returns a pair,
+	//the current state value, and a function that lets update our state
+	const [count, setCount] = useState(countInitial());
 
 	function decrementCount() {
 		setCount(prevCount => prevCount - 1);
